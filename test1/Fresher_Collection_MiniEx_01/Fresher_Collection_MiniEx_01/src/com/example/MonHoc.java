0cdc9484392a1de051aef9ec96a7f2a5bb2219f1
vuhongthien <vuhongthien18@gmil.com>
2022-06-03 23:11:16 +0700

    bổ sung bài tập

---
+++ test1/Fresher_Collection_MiniEx_01/Fresher_Collection_MiniEx_01/src/com/example/MonHoc.java
@@ -0,0 +1,45 @@
+package com.example;
+
+public class MonHoc {
+    private String ten;
+    private int tcLT;
+    private int tcTH;
+
+    public MonHoc(String ten, int tcLT, int tcTH) {
+        this.ten = ten;
+        this.tcLT = tcLT;
+        this.tcTH = tcTH;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTcLT() {
+        return tcLT;
+    }
+
+    public void setTcLT(int tcLT) {
+        this.tcLT = tcLT;
+    }
+
+    public int getTcTH() {
+        return tcTH;
+    }
+
+    public void setTcTH(int tcTH) {
+        this.tcTH = tcTH;
+    }
+
+    public boolean equals(Object that) {
+        //Hai mon duoc goi la bang nhau neu cung ten mon
+        if (that instanceof MonHoc) {
+            return ((MonHoc) that).ten.equals(this.ten);
+        }
+        return false;
+    }
+}
