116df82cb2f89e0e4ce7457fca728e97c1e49975
vuhongthien <vuhongthien18@gmil.com>
2022-06-07 12:06:47 +0700

    spring 1

--- test1/Topic02_Collection_Stream_Lamdas/4_09-LambdaBuiltIns/examples/SalesTxn09-Pkg/src/java/util/function/Consumer.java
+++
@@ -1,7 +0,0 @@
-package java.util.function;
-
-public interface Consumer<T> {
-    
-    public void Accept(T t);
-    
-}
--- test1/ex1/src/main/java/ex1/MainApplication.java
+++
@@ -1,7 +0,0 @@
-package ex1;
-
-public class MainApplication {
-    public static void main(String[] args) {
-
-    }
-}
